8bb92fac2fe3f94f8f0cbf6d5788ccd20e6286dd
najmus2808 <najmus2808@gmail.com>
2021-04-25 10:43:21 +1000

    calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,16 +3,33 @@ import './Cart.css';
 
 const Cart = (props) => {
     const cart = props.cart;
+    const cost = cart.reduce((sum,item) => sum + item.price,0);
+    let shippingCost = 0;
+    if(cost > 0 && cost < 50){
+        shippingCost = 15;
+    }
+    else if(cost > 50){
+        shippingCost = 10;
+    }
+
+    const total = cost + shippingCost;
+
+    let tax = 0;
+    if(cart.length != 0 ){
+        tax = (cost / 100) *10;
+    }
+
+    const grandTotal = Math.round(cost + shippingCost + tax);
     return (
         <div className="cart-container">
             <h3>Order Summary</h3>
             <h4>Items ordered:{cart.length}</h4>
             <div className="cart">
-            <h5>Items:</h5>
-            <h5>Shipping & Handling:</h5>
-            <h5>Total before tax:</h5>
-            <h5>Estimated Tax:</h5>
-            <h4>Order Total:</h4>
+            <h5>Items:${cost}</h5>
+            <h5>Shipping &amp; Handling:${shippingCost}</h5>
+            <h5>Total before tax:${cost}</h5>
+            <h5>Estimated Tax:${tax}</h5>
+            <h4>Order Total:${grandTotal}</h4>
             </div> 
         </div>
     );
